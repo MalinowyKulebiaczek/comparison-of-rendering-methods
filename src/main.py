cf024186b5bb188ed289deda9526a57dab4425a0
Hornisslaw <rkaczmarski.1998@gmail.com>
2023-01-06 15:24:42 +0100

    Improve multiprocessing for raytracing

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 from procedure import MainProcedure
 from scenecomponents.scene import Scene
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     file_dae = "src/scenes/sphere_and_cubes_colour.dae"
     resolution = 200
     samples = 8
     max_depth = 3
-    environment_map="src/scenes/skbx.jpg"
+    environment_map = "src/scenes/skbx.jpg"
 
-    scene = Scene.load(file_dae, 200)
     output_file = "out.png"
 
     MainProcedure(
@@ -16,6 +15,5 @@ if __name__ == '__main__':
         resolution=resolution,
         samples=samples,
         max_depth=max_depth,
-        environment_map=environment_map
+        environment_map=environment_map,
     ).renderRayTrace(output_file)
-
--- src/raytracing_scene_load.py
+++ src/raytracing_scene_load.py
@@ -20,112 +20,6 @@ def normalize(vector):
     return vector / np.linalg.norm(vector)
 
 
-def reflected(vector, axis):
-    return vector - 2 * np.dot(vector, axis) * axis
-
-
-def sphere_intersect(center, radius, ray_origin, ray_direction):
-    b = 2 * np.dot(ray_direction, ray_origin - center)
-    c = np.linalg.norm(ray_origin - center) ** 2 - radius ** 2
-    delta = b ** 2 - 4 * c
-    if delta > 0:
-        t1 = (-b + np.sqrt(delta)) / 2
-        t2 = (-b - np.sqrt(delta)) / 2
-        if t1 > 0 and t2 > 0:
-            return min(t1, t2)
-    return None
-
-
-def nearest_intersected_object(objects, ray_origin, ray_direction):
-    distances = [sphere_intersect(obj['center'], obj['radius'], ray_origin, ray_direction) for obj in objects]
-    nearest_object = None
-    min_distance = np.inf
-    for index, distance in enumerate(distances):
-        if distance and distance < min_distance:
-            min_distance = distance
-            nearest_object = objects[index]
-    return nearest_object, min_distance
-
-
-width = 300
-height = 200
-
-max_depth = 1
-
-camera = np.array([0, 0, 1])
-ratio = float(width) / height
-screen = (-1, 1 / ratio, 1, -1 / ratio)  # left, top, right, bottom
-
-light = {'position': np.array([5, 5, 5]), 'ambient': np.array([1, 1, 1]), 'diffuse': np.array([1, 1, 1]),
-         'specular': np.array([1, 1, 1])}
-
-objects = [
-    {'center': np.array([-0.2, 0, -1]), 'radius': 0.7, 'ambient': np.array([0.1, 0, 0]),
-     'diffuse': np.array([0.7, 0, 0]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5},
-    {'center': np.array([0.1, -0.3, 0]), 'radius': 0.1, 'ambient': np.array([0.1, 0, 0.1]),
-     'diffuse': np.array([0.7, 0, 0.7]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5},
-    {'center': np.array([-0.3, 0, 0]), 'radius': 0.15, 'ambient': np.array([0, 0.1, 0]),
-     'diffuse': np.array([0, 0.6, 0]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5},
-    # { 'center': np.array([0, -9000, 0]), 'radius': 9000 - 0.7, 'ambient': np.array([0.1, 0.1, 0.1]), 'diffuse': np.array([0.6, 0.6, 0.6]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5 }
-]
-
-
-# image = np.zeros((height, width, 3))
-# for i, y in enumerate(tqdm(np.linspace(screen[1], screen[3], height))):
-#     for j, x in enumerate(np.linspace(screen[0], screen[2], width)):
-#         # screen is on origin
-#         pixel = np.array([x, y, 0])
-#         origin = camera
-#         direction = normalize(pixel - origin)
-#
-#         color = np.zeros((3))
-#         reflection = 1
-#
-#         for k in range(max_depth):
-#             # check for intersections
-#             nearest_object, min_distance = nearest_intersected_object(objects, origin, direction)
-#             if nearest_object is None:
-#                 break
-#
-#             intersection = origin + min_distance * direction
-#             normal_to_surface = normalize(intersection - nearest_object['center'])
-#             shifted_point = intersection + 1e-5 * normal_to_surface
-#             intersection_to_light = normalize(light['position'] - shifted_point)
-#
-#             _, min_distance = nearest_intersected_object(objects, shifted_point, intersection_to_light)
-#             intersection_to_light_distance = np.linalg.norm(light['position'] - intersection)
-#             is_shadowed = min_distance < intersection_to_light_distance
-#
-#             if is_shadowed:
-#                 break
-#
-#             illumination = np.zeros((3))
-#
-#             # ambiant
-#             illumination += nearest_object['ambient'] * light['ambient']
-#
-#             # diffuse
-#             illumination += nearest_object['diffuse'] * light['diffuse'] * np.dot(intersection_to_light,
-#                                                                                   normal_to_surface)
-#
-#             # specular
-#             intersection_to_camera = normalize(camera - intersection)
-#             H = normalize(intersection_to_light + intersection_to_camera)
-#             illumination += nearest_object['specular'] * light['specular'] * np.dot(normal_to_surface, H) ** (
-#                     nearest_object['shininess'] / 4)
-#             #
-#             # # reflection
-#             # color += reflection * illumination
-#             # reflection *= nearest_object['reflection']
-#             #
-#             # origin = shifted_point
-#             # direction = reflected(direction, normal_to_surface)
-#
-#             image[i, j] = np.clip(illumination, 0, 1)
-#     # print("%d/%d" % (i + 1, height))
-#
-# plt.imsave('image.png', image)
-
 def ray_tracing_render(procedure: MainProcedure, max_depth):
     procedure.load_scene()
     procedure.load_background()
@@ -141,23 +35,23 @@ def ray_tracing_render(procedure: MainProcedure, max_depth):
     print(procedure.scene.camera.resolution)
 
     procedure.free_scene()
-
+    np.random.shuffle(rays)
     batches = np.array_split(rays, os.cpu_count())
-    pool = Pool(os.cpu_count())
     tasks = []
+    with Pool(processes=os.cpu_count()) as pool:
+        tasks = [
+            pool.apply_async(trace_ray_task, (batch, procedure)) for batch in batches
+        ]
+        pool.close()
+        pool.join()
+        for task in tasks:
+            for ((x, y), color) in task.get():
+                bitmap[y, x] = color
 
-    for batch in batches:
-        tasks.append(pool.apply_async(trace_ray_task, (batch, procedure)))
-    
-    pool.close()
-    pool.join()
-    for task in tasks:
-        for ((x,y), color) in task.get():
-            bitmap[y, x] = color
-            
     return bitmap
 
-def trace_ray_task(batch, procedure_template: MainProcedure):    
+
+def trace_ray_task(batch, procedure_template: MainProcedure):
     global PROCESS_PROCEDURE
 
     if PROCESS_PROCEDURE is None:
@@ -175,6 +69,7 @@ def trace_ray_task(batch, procedure_template: MainProcedure):
         colors.append(((x, y), np.clip(color, 0, 255)))
     return colors
 
+
 def trace_ray(procedure, ray):
     origin = ray.origin
     color = np.zeros(3)
@@ -182,13 +77,20 @@ def trace_ray(procedure, ray):
     if hit is None:
         color = background(procedure, ray)
     else:
-        light_shining_on_hit, direction_from_hitpoint_to_light = get_light_that_shines_on_point(procedure.scene,
-                                                                                                hit.coords)
+        (
+            light_shining_on_hit,
+            direction_from_hitpoint_to_light,
+        ) = get_light_that_shines_on_point(procedure.scene, hit.coords)
         if light_shining_on_hit is not None:
             hit_material = procedure.scene.get_material(hit.material_id)
             direction_from_point_to_camera = normalize(origin - hit.coords)
-            color = calculate_shading(hit_material, light_shining_on_hit, direction_from_hitpoint_to_light,
-                                    direction_from_point_to_camera, hit.normal)
+            color = calculate_shading(
+                hit_material,
+                light_shining_on_hit,
+                direction_from_hitpoint_to_light,
+                direction_from_point_to_camera,
+                hit.normal,
+            )
     return (color * 255).astype("uint8")
 
 
@@ -203,16 +105,27 @@ def get_light_that_shines_on_point(scene: Scene, point):
     return None, None
 
 
-def calculate_shading(hit_material, light, direction_from_point_to_light, direction_from_point_to_camera,
-                      normal_to_surface):
+def calculate_shading(
+    hit_material,
+    light,
+    direction_from_point_to_light,
+    direction_from_point_to_camera,
+    normal_to_surface,
+):
     illumination = np.zeros(3)
     # ambient
-    illumination += hit_material.ambient * light.color  # tbh should probably be sth like light.ambient but lgith seems noty to have such field
+    illumination += (
+        hit_material.ambient * light.color
+    )  # tbh should probably be sth like light.ambient but lgith seems noty to have such field
     # diffuse
-    illumination += hit_material.diffusion * np.dot(direction_from_point_to_light, normal_to_surface)
+    illumination += hit_material.diffusion * np.dot(
+        direction_from_point_to_light, normal_to_surface
+    )
     # specular
     H = normalize(direction_from_point_to_light + direction_from_point_to_camera)
-    illumination += hit_material.specular * np.dot(normal_to_surface, H) ** (hit_material.shininess / 4)
+    illumination += hit_material.specular * np.dot(normal_to_surface, H) ** (
+        hit_material.shininess / 4
+    )
 
     # TODO dorobic reflection
     # color = np.zeros(3)
@@ -223,7 +136,7 @@ def calculate_shading(hit_material, light, direction_from_point_to_light, direct
     return illumination
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     file_dae = "scenes/spheres_color_working.dae"
     resolution = 200
     samples = 8
@@ -238,7 +151,7 @@ if __name__ == '__main__':
         resolution=resolution,
         samples=samples,
         max_depth=max_depth,
-        environment_map=environment_map
+        environment_map=environment_map,
     )  # .render(output_file)
 
     procedure.load_scene()
