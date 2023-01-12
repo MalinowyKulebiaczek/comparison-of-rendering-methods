b9004f3d940c16e11821f601887d7a710e972f6c
Maciej <daraniel1000@gmail.com>
2023-01-12 18:06:23 +0100

    implement simple photon mapping

--- src/main.py
+++ src/main.py
@@ -2,11 +2,12 @@ from procedure import MainProcedure
 from scenecomponents.scene import Scene
 
 if __name__ == "__main__":
-    file_dae = "scenes/sphere_and_cubes_colour.dae"
+    file_dae = "src/scenes/sphere_and_cubes_colour.dae"
     resolution = 200
     samples = 8
     max_depth = 3
-    environment_map = "scenes/skbx.jpg"
+    environment_map = "src/scenes/skbx.jpg"
+    n_photons = 100000
 
     output_file = "out.png"
 
@@ -16,5 +17,6 @@ if __name__ == "__main__":
         samples=samples,
         max_depth=max_depth,
         environment_map=environment_map,
-        ).renderRayTrace(output_file)
+    ).renderPhotonMap(output_file, n_photons)
+        #).renderRayTrace(output_file)
     # ).renderPathTrace(output_file)
